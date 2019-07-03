37145afb4749a0466e223584ef137d6dfe6fb79f
rainerru <rainerru@stud.sbg.ac.at>
2019-07-03 12:40:35 +0200

    introduced a first test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod integration;
+pub mod integration;
 
 use integration::*;
 
--- tests/some_test.rs
+++ tests/some_test.rs
@@ -1,11 +1,11 @@
 use math;
+use math::integration;
 
 
 #[test]
 fn integrate_simple_tester()
 {
 	let res = integration::integrate_simple( &|x| x*x, 0.0, 3.0, 100);
-	assert!( res < 9.1 && res > 8.9 );
-	assert!( res >200.0);
+	assert!( res < 9.5 && res > 8.5 );
 }
 
